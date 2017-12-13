4668355ae5dcf74d48e7e0209c8fa8f0c1de0819
kohei yoshida <mc00102@MAC172.local>
2017-12-13 15:22:48 +0900

    update

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Beautiful Soupを使ってみる
+BSを使い、Qiitaでいいねした記事を切り抜きHTMLに整形。
--- scr.py
+++ scr.py
@@ -2,20 +2,22 @@ import urllib2
 import sys
 from bs4 import BeautifulSoup
 
+# 対象ページ読み込み（最初のページのみ）
 html = urllib2.urlopen("https://qiita.com/g-empr/like")
-
+# エラー吐くのでパーサー通す
 soup = BeautifulSoup(html, "html.parser")
-
+# 投稿者取得
 author_list = soup.select(".ItemLink__info a")
+# 記事取得
 article_list = soup.find_all("a", class_="u-link-no-underline")
-
+# 記事の抜き出しと整形
 for i in range(len(author_list)):
     print('<li>\n\t<span>{0}\'s entry: < /span >\n\t< a href="https://qiita.com{2}" > {1} < /a >\n < /li >'.format(
         author_list[i].string.encode('utf-8'),
         article_list[i].string.encode('utf-8'),
         article_list[i].get("href")
     ))
-
+# 別ファイルへの書き込み
 # f = open('test.txt', 'w')
 # f.write(title.encode('utf-8'))
 # f.close()
